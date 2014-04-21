5b790c71d26ec10b8eae929b9dd91bc6c60151e4
Christoph Hermann <schtoeffel@gmail.com>
2014-04-21 12:24:30 +0200

    test error object

--- index.js
+++ index.js
@@ -5,25 +5,29 @@
 
 //TODO: install error
 
-pub.install = function(packageName){
+pub.install = function(packageName) {
     var q = Q.defer();
-    npm.load(npm.config, function (err) {
-        npm.commands.install([packageName], function(data){
+    npm.load(npm.config, function(err) {
+        npm.commands.install([packageName], function(data) {
             q.resolve(data);
         });
     });
     return q.promise;
 };
 
-pub.search = function(keywords){
+pub.search = function(keywords) {
     var q = Q.defer();
     if (!_.isArray(keywords)) {
         keywords = [keywords];
     }
 
-    npm.load(npm.config, function (err) {
-        npm.commands.search(keywords, true, true, function(nully, data){
-            q.resolve(data);
+    npm.load(npm.config, function(err) {
+        npm.commands.search(keywords, true, true, function(error, data) {
+            if (error) {
+                q.reject(error);
+            } else {
+                q.resolve(data);
+            }
         });
     });
     return q.promise;

f6d67a966e00133fe85bb06ec46e38bd1f34564e
sprengr <sprengr@outlook.com>
2014-05-01 15:38:32 +0200

    update functionality, refactored

--- index.js
+++ index.js
@@ -4,54 +4,55 @@
     pub = module.exports;
 
 pub.install = function(keywords, options){
-    var q = Q.defer();
-    if (!_.isArray(keywords)) {
-        keywords = [keywords];
-    }
-    options = options || {};
-    
-    npm.load(function (err) {
-        if (options.global){
-            npm.config.set('global', true);
-        }
-        npm.commands.install(keywords, function(error, data){
-            if (error) {
-                q.reject(error);
-            } else {
-                q.resolve(data);
-            }
-        });
-    });
-    return q.promise;
+    return npmCommand('install', keywords, options);
 };
 
-pub.uninstall = function(keywords, options){
+pub.uninstall = function (keywords, options) {
+    return npmCommand('uninstall', keywords, options);
+};
+
+pub.update = function (keywords, options) {
+    return npmCommand('update', keywords, options);
+};
+
+function npmCommand(command, keywords, options) {
     var q = Q.defer();
-    if (!_.isArray(keywords)) {
-        keywords = [keywords];
-    }
     options = options || {};
-    
+
     npm.load(function (err) {
-        if (options.global){
-            npm.config.set('global', true);
+        npm.config.set('global', !!options.global);
+        if (keywords) {
+            keywords = ensureArray(keywords);
+            npm.commands[command](keywords, function (error, data) {
+                if (error) {
+                    q.reject(error);
+                } else {
+                    q.resolve(data);
+                }
+            });
+        } else {
+            npm.commands[command](function (error, data) {
+                if (error) {
+                    q.reject(error);
+                } else {
+                    q.resolve(data);
+                }
+            });
         }
-        npm.commands.uninstall(keywords, function(error, data){
-            if (error) {
-                q.reject(error);
-            } else {
-                q.resolve(data);
-            }
-        });
     });
     return q.promise;
-};
+}
+
+function ensureArray(objOrArray) {
+    if (!_.isArray(objOrArray)) {
+        objOrArray = [objOrArray];
+    }
+    return objOrArray;
+}
 
 pub.search = function(keywords) {
     var q = Q.defer();
-    if (!_.isArray(keywords)) {
-        keywords = [keywords];
-    }
+    keywords = ensureArray(keywords);
 
     npm.load(function(err) {
         npm.commands.search(keywords, true, true, function(error, data) {
--- test/index.js
+++ test/index.js
@@ -7,12 +7,12 @@ var npmUtils = require('../index'),
         load: function(cb) { cb(); }
     };
 
-require('mocha-as-promised')();
-
 describe('NpmUtils', function() {
     it('should offer install, search ', function() {
         npmUtils.should.respondTo('install');
         npmUtils.should.respondTo('search');
+        npmUtils.should.respondTo('uninstall');
+        npmUtils.should.respondTo('update');
     });
 
     describe('#install', function() {
@@ -68,6 +68,39 @@ describe('NpmUtils', function() {
         it('should call npm uninstall with keywords', function() {
             mocked.uninstall(['key', 'word']);
             return uninstallSpy.should.have.been.calledWith(['key', 'word']);
+        });
+    });
+
+    describe('#update', function () {
+        var mocked, updateSpy, configSpy;
+        beforeEach(function () {
+            updateSpy = sinon.spy();
+            configSpy = sinon.spy();
+            npmMock.commands.update = updateSpy;
+            npmMock.config = { set: configSpy };
+            mocked = proxyquire('../index', {
+                npm: npmMock
+            });
+        });
+
+        it('should call npm update', function () {
+            mocked.update('heinzel-generator-pg');
+            return updateSpy.should.have.been.calledWith(['heinzel-generator-pg']);
+        });
+
+        it('should set global', function () {
+            mocked.update('heinzel-generator-pg', { global: true });
+            return configSpy.should.have.been.calledWith('global', true);
+        });
+
+        it('should call npm update with keywords', function () {
+            mocked.update(['key', 'word']);
+            return updateSpy.should.have.been.calledWith(['key', 'word']);
+        });
+
+        it('should call npm update without keywords', function () {
+            mocked.update();
+            return updateSpy.should.have.been.called;
         });
     });
 
